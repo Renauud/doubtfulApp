e8367e54afbe7d580f9d2baed5b4d1068fc525b6
Renaud <rzell57@gmail.com>
2023-06-21 21:49:23 +0200

    ajout bgimage + button

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class flutterexp extends StatelessWidget {
-  const flutterexp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent[200],
+        title: const Text("feur"),
+      ),
+      body: Stack(
+        // utilisation de stack pour pouvoir mettre une bgimage
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const Center(
+            child: Column(
+              children: [
+                Text(
+                  "Welcome !",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 138, 64, 143),
+                    fontSize: 45,
+                  ),
+                ),
+                ButtonBar(
+                    buttonTextTheme:
+                        ButtonTextTheme.primary), //test à finir/changer
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutterexp/app.dart';
+import 'package:flutterexp/routes/routes.dart';
 
 void main() {
-  runApp(flutterexp());
+  runApp(const DoubtfulApp());
+}
+
+class DoubtfulApp extends StatelessWidget {
+  const DoubtfulApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: Routes.home,
+      routes: {
+        Routes.home: (context) => const HomePage(),
+      },
+    );
+  }
 }
